810a2d333bb302d48ed66edabcad3b703eb9f6f6
adityaingle2 <adityaingle2@gmail.com>
2025-12-02 10:28:11 -0800

    - minor space fix

--- src/TradingEngine.Core/Messaging/EngineMessage.cs
+++ src/TradingEngine.Core/Messaging/EngineMessage.cs
@@ -5,13 +5,13 @@ namespace TradingEngine.Core.Messaging;
 public abstract record EngineMessage(string Symbol, long Timestamp);
 
 public record NewOrderMessage(string Symbol, long Timestamp, Order Order)
-    : EngineMessage(Symbol, Timestamp);
+  : EngineMessage(Symbol, Timestamp);
 
 public record CancelOrderMessage(string Symbol, long Timestamp, Guid OrderId)
-    : EngineMessage(Symbol, Timestamp);
+  : EngineMessage(Symbol, Timestamp);
 
 public record UpdateOrderMessage(string Symbol, long Timestamp, Guid ExistingOrderId, decimal? NewPrice = null, int? NewQuantity = null)
-    : EngineMessage(Symbol, Timestamp);
+  : EngineMessage(Symbol, Timestamp);
 
 
 
